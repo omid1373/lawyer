427b37fcbd51846d43d1921331bec9db0c6dec18
omid1373 <omiddadvar94@gmail.com>
2020-08-23 17:22:37 +0430

    First Commit by crazy-ass Nigga :D

---
+++ database/migrations/2020_08_23_123348_create_addresses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("party_id")->nullable();
+            $table->timestamps();
+
+            $table->foreign("party_id")->references("id")->on("parties");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
