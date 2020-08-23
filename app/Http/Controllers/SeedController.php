f56da1719e98fd8ccf71d072be3af33a7f774c3b
omid1373 <omiddadvar94@gmail.com>
2020-08-23 18:41:51 +0430

    Shit ....

--- app/Http/Controllers/SeedController.php
+++ app/Http/Controllers/SeedController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class SeedController extends Controller
 {
     //
+    private $default = ['nationalId' , 'sacrifice' , 'certFirstPage' , //Party
+        'profile' /* User */  ,'education' /*Education*/ ,
+        'priority1', 'priority2', 'priority3', 'priority4', 'priority5', // Lawyer
+        'licenceFirstPage' , 'renewLicence' , // Licence
+        'judge' /*Judge*/ ,'certOffice' , 'vote' , // OfficeEmployee
+        'thesis' , 'otherPublication' /*Publication*/];
+
+    public function initialTypes(){
+        for($i=0 ; $i < count($this->default); $i++){
+            Type::create($this->default[$i]);
+        }
+    }
+
+    public function seed(){
+    }
 }
--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -8,6 +8,6 @@ class TestController extends Controller
 {
     //
     public function seed(){
-
+        
     }
 }
