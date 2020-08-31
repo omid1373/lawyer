64216191672c8ca04ebfd1e80b880ba6f8a031c6
omid1373 <omiddadvar94@gmail.com>
2020-08-31 14:23:59 +0430

    modified migrations :D

--- database/migrations/2020_08_23_124139_create_documents_table.php
+++ database/migrations/2020_08_23_124139_create_documents_table.php
@@ -15,7 +15,15 @@ class CreateDocumentsTable extends Migration
     {
         Schema::create('documents', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('type_id');
+            $table->string('name');
+            $table->string('path')->nullable();
+            $table->string('mime_type');
+            $table->nullableMorphs('documentable');
+            $table->dateTime('is_deleted')->nullable();
             $table->timestamps();
+
+            $table->foreign("type_id")->references("id")->on("types");
         });
     }
 
